684d16e36a109bf003899978d6c9ff2ace60414b
Sara Shafiei Sarvestani <sss17@tu-clausthal.de>
2026-02-04 18:00:14 +0100

    Feat: Enable password protection on main entry point

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,12 @@
 
-  import { createRoot } from "react-dom/client";
-  import App from "./App.tsx";
-  import "./index.css";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-  createRoot(document.getElementById("root")!).render(<App />);
-  
+import { PasswordProtection } from "./components/PasswordProtection";
+
+createRoot(document.getElementById("root")!).render(
+  <PasswordProtection>
+    <App />
+  </PasswordProtection>
+);
