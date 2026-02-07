44ac05d72a682229bdf2c0822280bf638dede2e1
Sara Shafiei Sarvestani <sss17@tu-clausthal.de>
2026-02-07 18:04:10 +0100

    feat: Remove password protection to make website public

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-import { PasswordProtection } from "./components/PasswordProtection";
-
 createRoot(document.getElementById("root")!).render(
-  <PasswordProtection>
-    <App />
-  </PasswordProtection>
+  <App />
 );
